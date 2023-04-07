c2c73be8be018db993b6096d0ee5eef6fe4aa1e0
Olude david <odavidolumide@gmail.com>
2023-04-07 12:00:42 +0200

    React-static-app-commit

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Card from "./Card";
+import data from "../data";
+import "../style.css";
+
+export default function App() {
+  const cards = data.map((item) => {
+    return (
+      <Card
+        key={item.id}
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+        openSpots={item.openSpots}
+      />
+    );
+  });
+
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <section className="cards-list">{cards}</section>
+    </div>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../style.css";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="../images/airbnb_logo.png" className="nav--logo" />
+    </nav>
+  );
+}
